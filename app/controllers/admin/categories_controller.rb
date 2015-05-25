d025030d7e0b345a7dcd1516ff01ec44049ef957
Dao Minh Duc <brokenheart1712@gmail.com>
2015-05-25 14:03:15 +0700

    Categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,49 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+    @categories = Category.paginate page: params[:page],
+    per_page: Settings.page_size
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      flash[:success] = t :success
+      redirect_to admin_categories_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @category = Category.find params[:id]
+  end
+
+  def update
+    @category = Category.find params[:id]
+    if @category.update_attributes category_params
+      flash[:success] = t :success
+      redirect_to admin_categories_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category = Category.find params[:id]
+    if @category.destroy
+      flash[:success] = t :success
+    else
+      flash[:danger] = t :fail
+    end
+    redirect_to admin_categories_path
+  end
+
+  private
+  def category_params
+    params.require(:category).permit :name
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  root "static_pages#home"
+
+  namespace :admin do
+    root "categories#index"
+    resources :categories
+  end
 end
