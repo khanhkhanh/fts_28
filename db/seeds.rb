d35fee66e325ca5ba365978570cc5d65b2a7e007
khanhkhanh <ngo.duy.khanh@framgia.com>
2015-05-28 09:42:29 +0700

    Create, do (without countdown), review (without marking) an exam (authorisation with CanCanCan) functions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-User.delete_all
-
 User.create!(email: 'framgia.test.app.28@gmail.com',
              password: '08011992',
              password_confirmation: '08011992',
              admin: true)
+
+User.create!(email: "admin@gmail.com",
+             password: "adminadmin",
+             password_confirmation: "adminadmin",
+             admin: true)
+
+User.create!(email: "khanhkhanh@gmail.com",
+             password: "khanhkhanh",
+             password_confirmation: "khanhkhanh",
+             admin: false)
 
 49.times do |n|
   name  = Faker::Name.name
@@ -13,21 +21,25 @@ User.create!(email: 'framgia.test.app.28@gmail.com',
               password: password,
               password_confirmation: password,
               admin: false)
+
+Category.create!(name: "MySQL")
+Category.create!(name: "Git")
+Category.create!(name: "Ruby on Rails")
+
+_MySQLQuestions = Category.find(1).questions
+3.times do |n|
+  content = "MySQL'sAnswer#{n+1}"
+  _MySQLQuestions.each {|question| question.answers.create! content: content, correct: n == 1}
 end
 
-5.times do |n|
-  name = "Category #{n+1}"
-  Category.create!(name: name)
+_GitQuestions = Category.find(2).questions
+3.times do |n|
+  content = "Git'sAnswer#{n+1}"
+  _GitQuestions.each {|question| question.answers.create! content: content, correct: n == 1}
 end
 
-categories = Category.all
-30.times do |n|
-  content = "Question #{n+1}"
-  categories.each {|category| category.questions.create! content: content}
-end
-
-questions  = Question.order(:created_at).all
-4.times do |n|
-  content = "Answer #{n+1}"
-  questions.each {|question| question.answers.create! content: content, correct: n == 1}
+_RubyOnRailsQuestions = Category.find(3).questions
+3.times do |n|
+  content = "RubyOnRails'Answer#{n+1}"
+  _RubyOnRailsQuestions.each {|question| question.answers.create! content: content, correct: n == 1}
 end
