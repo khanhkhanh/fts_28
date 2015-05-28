0cc200ed0116c2a7e784e6815e609763b3089537
khanhkhanh <ngo.duy.khanh@framgia.com>
2015-05-28 11:40:25 +0700

    Mark an exam

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -5,6 +5,7 @@ class Exam < ActiveRecord::Base
   has_many :questions, through: :results
 
   before_save :make_random_questions, unless: :is_done?
+  before_update :mark_the_exam, :update_correct_answers, if: :is_done?
 
   accepts_nested_attributes_for :results, allow_destroy: true, 
                                           reject_if: :all_blank
@@ -15,6 +16,18 @@ class Exam < ActiveRecord::Base
   def make_random_questions
     @questions = category.questions.random_questions
     @questions.each {|question| results.build question: question}
+  end
+
+  def mark_the_exam
+    self.mark = results.select do |result|
+      Answer.find(result.answer_id).correct? if result.answer_id?
+    end.count
+  end
+
+  def update_correct_answers
+    results.each do |result|
+      result.answer_id? ? result.correct = Answer.find(result.answer_id).correct? : false
+    end
   end
 
   def is_done?
